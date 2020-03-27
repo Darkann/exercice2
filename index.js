9dc00f48e49a96d134d2afd728ff83b6803f363e
Darkann <60514789+Darkann@users.noreply.github.com>
2020-03-27 13:17:37 +0100

    routage creation articles et envoie bdd  + divers rectif html

--- index.js
+++ index.js
@@ -13,3 +13,36 @@ app.listen(3000, () =>  {
 
 app.use(express.static("./public"));
 app.use(express.urlencoded({extended: true}));
+
+app.get("/api/articles", (req, res) => {
+    const sqlConnection = mysql.createConnection(sqlConfig);
+    sqlConnection.query(
+        "SELECT id, title, content, author, created_at FROM node_articles WHERE id = 1 LIMIT 1",
+        (error, result) => {
+            if (error) {
+                console.log("ERROR :", error.code);
+            } else {
+                res.send(result[0]);
+            }
+            sqlConnection.end();
+        });
+});
+app.route("/api/articles/create")
+    .get((req, res) => res.status(503).send({ status: "ERROR" }))
+    .post((req, res) => {
+        console.log(req.body);
+        const sqlConnection = mysql.createConnection(sqlConfig);
+        sqlConnection.query(
+            "INSERT INTO node_articles VALUES (NULL, ?, ?, ?, ?)",
+            [req.body.title, req.body.content, req.body.author, req.body.date],
+            (error, result) => {
+                if (error) {
+                    console.log("ERROR :", error.code);
+                    res.status(503).send({ status: "ERROR" });
+                } else {
+                    console.log(result);
+                    res.send({ status: "OK" });
+                }
+                sqlConnection.end();
+            });
+    });
